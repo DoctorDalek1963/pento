f9dec66756c761e661501a0cd7acbf9bb1b689e4
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-07-10 13:27:13 +0100

    Allow user to win and reset WrongLive game

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,27 +2,44 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       target: :rand.uniform(10),
+       has_won: false
+     )}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Your score: <%= @score %></h1>
     <h2><%= @message %></h2>
-    <h2>
-      <%= for n <- 1..10 do %>
-        <.link href="#" phx-click="guess" phx-value-number={n}>
-          <%= n %>
-        </.link>
-      <% end %>
-    </h2>
+    <%= if @has_won do %>
+      <.link navigate={~p"/guess"}><strong>Reset</strong></.link>
+    <% else %>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <.link href="#" phx-click="guess" phx-value-number={n}>
+            <%= n %>
+          </.link>
+        <% end %>
+      </h2>
+    <% end %>
     """
   end
 
   def handle_event("guess", %{"number" => number}, socket) do
-    message = "Your guess: #{number}. Wrong. Guess again:"
-    score = socket.assigns.score - 1
+    if number == to_string(socket.assigns.target) do
+      message = "Your guess: #{number}. Correct! You win!"
+      score = socket.assigns.score + 1
 
-    {:noreply, assign(socket, score: score, message: message)}
+      {:noreply, assign(socket, score: score, message: message, has_won: true)}
+    else
+      message = "Your guess: #{number}. Wrong. Guess again:"
+      score = socket.assigns.score - 1
+
+      {:noreply, assign(socket, score: score, message: message)}
+    end
   end
 end
