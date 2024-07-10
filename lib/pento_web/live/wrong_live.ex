8c318aeec6750a397b872197fbb622bf0819ad9d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-07-10 13:01:10 +0100

    Start WrongLive module

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,28 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h2><%= @message %></h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <.link href="#" phx-click="guess" phx-value-number={n}>
+          <%= n %>
+        </.link>
+      <% end %>
+    </h2>
+    """
+  end
+
+  def handle_event("guess", %{"number" => number}, socket) do
+    message = "Your guess: #{number}. Wrong. Guess again:"
+    score = socket.assigns.score - 1
+
+    {:noreply, assign(socket, score: score, message: message)}
+  end
+end
